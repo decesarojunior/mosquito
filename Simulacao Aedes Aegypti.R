e7a38737b059dba11030c04c65acdb0a91d2bef2
Thiago Berton Ferreira <thiago.bertonf@gmail.com>
2018-11-29 17:52:26 -0200

    Update Simulacao Aedes Aegypti.R

--- Simulacao Aedes Aegypti.R
+++ Simulacao Aedes Aegypti.R
@@ -22,137 +22,159 @@ setClass("indbasedModel",
 ##-------------------------------------------------------------------
 
 ibm_aedes = new("indbasedModel",
-                  main = function(time, init, parms) { #para cada vez q ela ser chamada
-                    init = live(init, parms)
-                    init = survive(init, parms)
-                    init = hatch(init, parms)
-                    #init = death(init)
-                    init  #retorna todos os inits acima
-                    #qual seria substituido
-                  },
-                  equations = list(
-                    newaedes = function(n) { 
-                      if (n>0) { #se nasceu no dia
-                        data.frame(age = rep(0, n), fase = 1, eggs = 0, gdd = 0, ovoposicao = 0) 
-                        
-                      } else { #se nao nasceu no dia
-                        NULL
-                      }
-                    },
-
-                    live = function(inds, parms){
-                      with(parms,{
-                        ninds       = nrow(inds) #fala o numero de individuos atraves da  contagem do numero de linhas na tabela
-
-                        if (ninds == 0){  #caso todos individuos tenham morrido, printa no console a seguinte mensagem
-                        
-                          warning("All individuals died - Game Over")  
-                          
-                        }
-                       
-                        #verifica se tem nivel minimo de 10mL de agua
-                        inds[inds$fase==1 & parms$agua >= 10,'gdd'] <- inds[inds$fase==1 & parms$agua >= 10,'gdd'] + (parms$temp-parms$tb[1])
-                        
-                        #para o age caso nao tem nivel minimo de 10mL de agua
-                        inds[inds$fase==1 & parms$agua >=10, 'age'] <- inds[inds$fase==1 & parms$agua >=10, 'age'] + DELTAT
-                        
-                        inds[inds$fase!=1, 'age'] <- inds[inds$fase!=1, 'age'] + DELTAT # os demais aumentam
-                        
-                        inds[inds$fase==2,'gdd'] <- inds[inds$fase==2,'gdd'] + (parms$temp-parms$tb[2])
-                        
-                        inds[inds$fase==3,'gdd'] <- inds[inds$fase==3,'gdd'] + (parms$temp-parms$tb[3])
-                        
-                        inds[inds$fase==4,'gdd'] <- inds[inds$fase==4,'gdd'] + (parms$temp-parms$tb[4])
-                        
-                        OpL  <- which(inds$fase == 1 & inds$gdd>=305.8) #ovo para larva
-                        inds$fase[OpL] = 2 #troca de fase
-                        inds$gdd[OpL] = 0 #zera graus dia de quem mudou de fase
-                        LpP  <- which(inds$fase == 2 & inds$gdd>=433.5) #Larva para Pupa
-                        inds$fase[LpP] = 3 #troca de fase
-                        inds$gdd[LpP] = 0 #zera graus dia de quem mudou de fase
-                        PpA  <- which(inds$fase == 3 & inds$gdd>=465.6) #Pupa para Adulto
-                        inds$fase[PpA] = 4 #troca de fase
-                        inds$gdd[PpA] = 0 #zera graus dia de quem mudou de fase
-                        #1204.9
-                        
-                        #****fazer subset que elimine os individuos em fases aquaticas caso nao ha 10mL****
-                        #subset(inds, inds$fase==2 | inds$fase==3 & parms$agua <=10)
-                        
-                        inds #retorna os resultados
-                      })},
-                    survive  = function(inds, parms){ 
-                      diasfase = c(4.1, 6.6, 2.4, 12.4) # médias de dias 
-                      probmorte = c(0.383, 0.075, 0.062, 0.02) # probabilidade de morte em cada fase
-                      probmorte = probmorte / diasfase # ajustado por que os indivíduos são testados para morte a cada dia, e não a cada fase
-                      subset(inds, runif(inds$age) > probmorte[inds$fase] & inds$age <= parms$maxage & parms$agua != 0) #|
-                               #runif(inds$age) > probmorte[inds$fase] & inds$age <= parms$maxage & parms$agua == 0 & inds$fase == 1)
-                      
-                    },
-                    hatch = function(inds, parms) {
-                      newinds = NULL
-                      with(parms, {
-                        
-                        #***ver maneira de fazer o max de ovosicao randomico para cada individuo***
-                        # maxovoposicao = runif(1,1,5)
-                        #ovoposicao < 5 ou ovoposicao <= maxovoposicao
-                        
-                       # inds[inds$fase==4,'eggs'] <- inds[inds$fase==4,'eggs'] + round(runif(1,40,200))
-                        inds[inds$fase==4 & inds$ovoposicao < 5,'eggs'] <- inds[inds$fase ==4 & inds$ovoposicao < 5,'eggs'] + round(runif(1,40,200))
-                        #verifica se o individuo esta na fase 4 e gera um numero random de eggs de 40-200
-                        
-                        inds[inds$fase==4  & inds$ovoposicao <= 5,'ovoposicao'] <- inds[inds$fase==4 & inds$ovoposicao <= 5,'ovoposicao'] + 1
-                        #verifica se chegou no limite maximo de ovoposicao, caso sim, o mosquito nao gera mais ovos
-                        
-                       #print(inds)
-                        #print("**********")
-                        
-                        have.neo  = 0
-                        new.neo   = 0
-                        have.neo  = which(inds$eggs > 0)
-                        
-                        eggs      = inds$eggs[have.neo] #seleciona somente os ind que tem eggs
-                        #print(eggs)
-                        new.neo   = sum(eggs) # soma o total de ovos 
-                        #print("Antes ")
-                        #print(new.neo)
-                        inds$eggs[have.neo]   = 0
-                       #print("Depois ")
-                        #print(new.neo)
-                         # zera os q tinham ovos
-                        newinds = newaedes(new.neo) # cria novos mosquitos 
-                        rbind(inds, newinds) # adiciona na lista (data.frame) de individuos
-                      })
+                main = function(time, init, parms) { 
+                  
+                  init = adult(init, parms)
+                  init = development(init, parms)
+                  init = pupa(init, parms)
+                  init = larva(init, parms)
+                  init = egg(init, parms)
+                  init = survive(init, parms)
+                  
+                },
+                equations = list(
+                  newaedes = function(n) { 
+                    if (n>0) { 
+                      data.frame(age = rep(0, n), phase = 1, eggs = 0, gdd = 0, oviposition = 0) 
+                      #create data.frame with 5 columns: age, phase, eggs, gdd, oviposition
+                    } else { 
+                      NULL
                     }
-                  ),parms = list(
-                    maxage      = 44,       #dias
-                    temp        = 40, #runif(1,25,40),       
-                    agua = 10, #runif(1,8,10),
-                    tb = c(2.9, 8.6, 10.8, 3.43)
-                  ),                      
-                  init = data.frame(age=0, fase=4, eggs=0, gdd=0, ovoposicao = 0),
-                  times = c(from=0, to=60, by=1),
-                  solver = "myiteration"
+                  },
+                  #we are using the opposite cycle direction (adult - pupa - larva - egg) 
+                  #because it will prevent an individual to enter two stages in a single day
+                  egg = function(inds, parms){
+                    with(parms,{
+                      inds[inds$phase==1 & parms$water >= 10,'gdd'] <- inds[inds$phase==1 & parms$water >= 10,'gdd'] + (parms$temp-parms$bt[1])
+                      #checks if water level is at least 10mL
+                      
+                      inds[inds$phase==1 & parms$water >=10, 'age'] <- inds[inds$phase==1 & parms$water >=10, 'age'] + DELTAT
+                      #if water < 10mL egg age won't change
+                      
+                      EtL  <- which(inds$phase == 1 & inds$gdd>=parms$k[1]) #egg to larva
+                      inds$phase[EtL] = 2 #troca de phase
+                      inds$gdd[EtL] = inds$gdd[EtL] - parms$k[1] 
+                      #inds who evolved will most likely surpass the minimum gdd necessary to next phase 
+                      #in this case we need to add the excess to the next stage 
+                      
+                      inds
+                    })},
+                  larva = function(inds, parms){
+                    with(parms,{
+                      inds[inds$phase==2, 'age'] <- inds[inds$phase==2, 'age'] + DELTAT 
+                      
+                      inds[inds$phase==2,'gdd'] <- inds[inds$phase==2,'gdd'] + (parms$temp-parms$bt[2])
+                      
+                      LtP  <- which(inds$phase == 2 & inds$gdd>=parms$k[2]) #Larva to Pupa
+                      inds$phase[LtP] = 3 #change phase
+                      inds$gdd[LtP] = inds$gdd[LtP] - parms$k[2] 
+                      #inds who evolved will most likely surpass the minimum gdd necessary to next phase 
+                      #in this case, we need to add the excess to the next stage 
+                      
+                      inds
+                      #subset
+                    })},
+                  pupa = function(inds, parms){
+                    with(parms,{
+                      inds[inds$phase==3, 'age'] <- inds[inds$phase==3, 'age'] + DELTAT 
+                      
+                      inds[inds$phase==3,'gdd'] <- inds[inds$phase==3,'gdd'] + (parms$temp-parms$bt[3])
+                      PtA  <- which(inds$phase == 3 & inds$gdd>=parms$k[3]) #Pupa to adult
+                      inds$eggs[PtA] = round(runif(inds$eggs,200,300))[PtA] 
+                      inds$phase[PtA] = 4 #change phase
+                      inds$gdd[PtA] = inds$gdd[PtA] - parms$k[3]  
+                      #inds who evolved will most likely surpass the minimum gdd necessary to next phase 
+                      #in this case we need to add the excess to the next stage 
+                       
+                      #insert a random number between 200 and 300 when the pupa is ready to become an adult
+                      # ...[PtA] returns TRUE or FALSE and indicate which individuals are ready
+                    
+                      inds
+                    })},
+
+                  
+                  
+                     survive  = function(inds, parms){ 
+                  #     diasphase = c(4.1, 6.6, 2.4, 12.4) # médias de dias 
+                      probmorte = c(0.383, 0.075, 0.062, 0.02) # probabilidade de morte em cada phase
+                      probmorte <- probmorte / parms$k
+
+                      
+                      #eggsLive <-  subset(inds[inds$phase==1,],  runif(age) > (gdd*probmorte[1]))
+                      #larvaLive <- subset(inds[inds$phase==2,],  runif(age) > (gdd*probmorte[2]))
+                      #pupaLive <-  subset(inds[inds$phase==3,],  runif(age) > (gdd*probmorte[3]))
+                      #adultLive <- subset(inds[inds$phase==4,],  runif(age) > (gdd*probmorte[4]) & gdd<parms$k[4])
+                      
+                     #inds <- rbind(eggsLive,larvaLive,pupaLive,adultLive)
+                     
+                     inds
+                  
+                },
+                     adult = function(inds, parms){
+                       with(parms,{
+                         inds[inds$phase==4, 'age'] <- inds[inds$phase==4, 'age'] + DELTAT 
+                         inds[inds$phase==4, 'gdd'] <- inds[inds$phase==4, 'gdd'] + (parms$temp-parms$bt[4])
+                         inds
+                       })},
+                     development = function(inds, parms){
+                       newinds = NULL
+                       with(parms,{
+                         have.neo  = which(inds$eggs > 0 & (inds$age %% 3 == 0))
+                         oviposition = round(runif(inds$eggs[have.neo],30,50))
+                         inds$oviposition[have.neo] = inds$oviposition[have.neo] + 1
+                         eggs = inds$eggs[have.neo]
+                         #print("oviposition ")
+                         #print(oviposition)
+                       #  print(inds)
+                         #print("**********")
+                         #print("eggs ")
+                         #print(eggs)
+                         ImmatureEggs = ifelse(length(eggs) > 0, mapply(max, (eggs - oviposition), 0), 0) #problema
+                         new.neo = sum(eggs - ImmatureEggs)
+                         
+                         inds$eggs[have.neo] <- ImmatureEggs
+                         
+                         newinds = newaedes(new.neo) #create new inds according to the sum(eggs)
+                         rbind(inds, newinds) # bind these new inds to the data.frame
+                       })}
+                    
+                     
+                ),parms = list(
+                  maxage      = 44,       #days
+                  temp        = 50, #runif(1,25,40),       
+                  water = 10, #runif(1,8,10),
+                  bt = c(2.9, 8.6, 10.8, 3.43),
+                  k = c(305.8, 433.5, 465.6, 660) #added a new parameter (660) for the adult mosquitos 
+                  #probmorte = c(0.383, 0.075, 0.062, 0.02)
+                ),                      
+                init = data.frame(age=0, phase=4, eggs=150, gdd=0, oviposition = 0),
+                times = c(from=0, to=60, by=1),
+                solver = "myiteration"
 )
+#alterar o metodo de morte - ver como resolver o problema
+#adicionar o grupo de individuos conforme o campina grande - ver porque entra no estado de pupa mesmo sendo egg
+#alterar graficos, adicionar numero de individuos em cada fase e o numero de ovos que cada adulto tem
 
 ##-------------------------------------------------------------------
 ## 1.3 Optionally define a user provided solver function that stores
 ##     the data more model specific than the default iteration
 ##-------------------------------------------------------------------
 
-#aqui possivelmente esta o problema
+
 myiteration = function(y, times = NULL, func = NULL, parms = NULL,
                        animate = FALSE, ...) {
   observer = function(res) { 
     # eggs, size, age, eggage
+   # t = length(which(res$phase==4))
     number   = nrow(res)
-    meanfase = mean(res$fase)
+    meanphase = mean(res$phase)
     meanage  = mean(res$age)
     meaneggs = mean(res$eggs)
     #print("res$eggs:")
     #print(res$eggs)
-   #print("mean eggs:")
+    #print("mean eggs:")
     #print(meaneggs)
-    c(number=number, meanfase=meanfase, meanage=meanage, meaneggs=meaneggs)
+    c(number=number, meanphase=meanphase, meanage=meanage, meaneggs=meaneggs)
   }
   init              = y@init
   times             = fromtoby(y@times)
@@ -165,7 +187,7 @@ myiteration = function(y, times = NULL, func = NULL, parms = NULL,
   parms$DELTAT = 0
   res = observer(init)
   out = res
-  for (i in 2:length(times)) { #comeca em 2 porque vetores comecam em 1 inves de 0
+  for (i in 2:length(times)) { 
     time = times[i]
     parms$DELTAT = times[i] - times[i-1]
     init = func(time, init, parms)
@@ -187,7 +209,7 @@ setMethod("plot", c("indbasedModel", "missing"), function(x, y, ...) {
   on.exit(par(opar))
   par(mfrow=c(2, 2))
   o = out(x)
-
+  
   plot(o$times, o$meanage,  type="l", xlab="Time", ylab="Mean age (d)")
   plot(o$times, o$meaneggs, type="l", xlab="Time", ylab="Eggs per individual")
   plot(o$times, o$number,   type="l", xlab="Time", ylab="Abundance")
@@ -201,4 +223,3 @@ setMethod("plot", c("indbasedModel", "missing"), function(x, y, ...) {
 ##solver(ibm_aedes) = "myiteration"
 ibm_aedes = sim(ibm_aedes)
 plot(ibm_aedes)
-
